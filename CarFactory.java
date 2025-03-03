f902bdf8c71b384fdabe0bd5ec809a95e1017121
Danne <daniel.kindvall05@gmail.com>
2025-03-03 13:46:13 +0100

    add observer interface and observeable class

--- CarFactory.java
+++ CarFactory.java
@@ -1,17 +1,5 @@
 public class CarFactory {
-    public Car createCar(String carType) {
-        switch (carType) {
-            case "Volvo240":
-                return new Volvo240();
-            case "Saab95":
-                return new Saab95();
-            case "Scania":
-                return new Scania();
 
-            default:
-                throw new java.lang.RuntimeException("Not an existing car");
-        }
-    }
 
     public Volvo240 createVolvo240() {
         return new Volvo240();
